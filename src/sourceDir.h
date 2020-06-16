68c6a96e3bf6ae4fdee91dd1ed97b02fbb8f8fc2
NPIPHI <nwp2@illinois.edu>
2020-06-16 14:20:29 -0500

    error on unset path

--- src/sourceDir.h
+++ src/sourceDir.h
@@ -7,6 +7,7 @@
 
 //emscripten file system does not respect the directory of the executable like the native c++ does,
 //this is a workaround, but there are probably better systems
+#error replace the path below with the correct path for your system
 #define ROOTDIRECTORY std::string("C://your/path/to/minimumGL/")
 
 
